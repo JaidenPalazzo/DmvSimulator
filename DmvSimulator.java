e3548eacc100b416e1d7664b8c7d686439d29d02
JaidenPalazzo <jaiden.palazzo@quinnipiac.edu>
2025-02-26 19:40:48 -0500

    conversation with user

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,7 +1,24 @@
 package DmvSimulator;
 
+import java.util.Random;
+
 public class DmvSimulator {
     public static void main(String [] args){
+        System.out.println("Welcome to the DMV!!"); 
         
+        Random random = new Random(); 
+        int num = random.nextInt(100) +1; 
+        System.out.println("Your number is " + num + ". Please wait until your number is called. "); 
+
+        for (int i = num + 1; i <= 100; i++) {
+            System.out.println("Number " + i + " Please come up to the front"); 
+        }
+
+        for (int i = 1; i < num; i ++) {
+            System.out.println("Number "+ i+ " Please come to the front"); 
+        }
+
+        System.out.println("you do not have the required paperwork"); 
+
     }
 }
